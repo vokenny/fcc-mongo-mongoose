ec80c64ab8f8a567deda6bdbdfd59624e629a9af
Kenny Vo <kennyvo208@gmail.com>
2023-08-09 15:50:23 +0100

    fix: implement removeManyPeople

--- myApp.js
+++ myApp.js
@@ -112,8 +112,13 @@ const removeById = (personId, done) => {
 
 const removeManyPeople = (done) => {
   const nameToRemove = 'Mary';
+  const targetPeople = { name: nameToRemove };
 
-  done(null /*, data*/);
+  Person.remove(targetPeople, function (err, result) {
+    if (err) return console.error(err);
+
+    done(null, result);
+  });
 };
 
 const queryChain = (done) => {
